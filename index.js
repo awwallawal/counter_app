66734e9d540ee7082dc4dabc085789e069d413b2
Awwal Lawal <awwallawal@gmail.com>
2022-06-12 22:21:59 +0100

    Designed a web browser based counter app
    This project is based on the tutorial from freeCodeCamp as taught by the Owner of Scrimba.com.

---
+++ index.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+let peopleEntered = document.getElementById('numbers-of-people');
+
+let Saving = document.getElementById('saving');
+
+function increment () {
+ count = count + 1;
+ peopleEntered.innerText = count;
+ alert('New Count: ' + count);
+};
+
+function save () {
+ Saving.textContent = Saving.textContent + count  +  ' --' + '   ';
+ alert ('Current Save: ' + count);
+ count = 0;
+}
