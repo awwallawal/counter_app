2afb6754f20c7a7281e0e177ce59ab1fabca23d4
Awwal Lawal <awwallawal@gmail.com>
2022-06-12 23:09:38 +0100

    Worked on the save function

---
+++ ft.js
@@ -0,0 +1,6 @@
+let count = 0;
+function counting () {
+ count += 1;
+ console.log(count);
+};
+counting();
--- index.js
+++ index.js
@@ -7,11 +7,12 @@ let Saving = document.getElementById('saving');
 function increment () {
  count = count + 1;
  peopleEntered.innerText = count;
- alert('New Count: ' + count);
+ // alert('New Count: ' + count);
 };
 
 function save () {
  Saving.textContent = Saving.textContent + count  +  ' --' + '   ';
- alert ('Current Save: ' + count);
+ // alert ('Current Save: ' + count);
  count = 0;
+ peopleEntered.innerText = count;
 }
