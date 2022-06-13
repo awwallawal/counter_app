d0a55a0f6c848fb520cc7a35190e2290166d783b
Awwal Lawal <awwallawal@gmail.com>
2022-06-13 10:07:12 +0100

    Using An IIFE
    In order to cricumvent the declaration of the variables at the global scope, I used the concept of Immediately Invockable Function Expression to declare the variables at the local level.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* 
 let count = 0;
 
 let peopleEntered = document.getElementById('numbers-of-people');
@@ -15,4 +16,25 @@ function save () {
  // alert ('Current Save: ' + count);
  count = 0;
  peopleEntered.innerText = count;
-}
+} 
+
+*/
+
+// The above code would work but we can modify the code by writting an IIFE (Immediately Invockable Function Expression) to circumvent the writing of the code at the global scope level and make it work under its local scope which is the best practice when writting codes. 
+
+let counterApp = (function counter(){
+ let count = 0;
+ let peopleEntered = document.getElementById('numbers-of-people');
+ let Saving = document.getElementById ('saving');
+ return{
+  increment: function () {
+   count += 1;
+   peopleEntered.textContent = count;
+  },
+  save: function () {
+   Saving.textContent = Saving.textContent + count + '   '+ '--- ';
+   count = 0;
+   peopleEntered.textContent = 0;
+  }
+ }
+}) ();
